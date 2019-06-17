483c8fd4f2bd303ab8a3570932eaaab90df1a311
yffd123 <yff571856@163.com>
2019-06-17 20:10:04 +0800

    sd

--- sd.py
+++ sd.py
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
-var01_split = str01.split()
-print(str01_split, type(str01_split))
+var01_split = var01.split()
+print(var01_split, type(var01_split))
 =======
 var01_split = var01.split()
 print(var01_split, type(var01_split))
